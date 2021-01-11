9bd23fab827bf77e7c80b9023135b700c462a618
MatsAndT <matsandreas@tonnesland.name>
2021-01-11 11:17:01 +0100

    init

--- data.py
+++ data.py
@@ -1,6 +1,23 @@
+import os
+import sqlite3
+from sqlite3 import Error
+import logging
+from traceback import format_exc
+
+logger = logging.getLogger('astro')
+
 class Data:
-    def __init__(self):
-        pass
+    def __init__(self, db_path):
+        logger.info('Class DataManager init')
+
+        self.db_name = db_path
+
+        try:
+            self.conn = sqlite3.connect(os.path.abspath(self.db_name))
+        except Error as e:
+            logger.critical('Cannot connect to db: {}'.format(format_exc()))
+
+        logger.debug('Class __init__ end')
     
     def create_table(self):
         pass
