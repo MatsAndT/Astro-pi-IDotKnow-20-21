04ce6dee508059fad8158e15fbf7e2b322a70889
MatsAndT <matsandreas@tonnesland.name>
2021-01-11 10:48:52 +0100

    mockup

---
+++ data.py
@@ -0,0 +1,12 @@
+class Data:
+    def __init__(self):
+        pass
+    
+    def create_table(self):
+        pass
+
+    def insert(self, data):
+        pass
+
+    def close(self):
+        pass
