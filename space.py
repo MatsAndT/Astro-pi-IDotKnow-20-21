306c98475b222e1254f62e83d414836df546fde2
MatsAndT <matsandreas@tonnesland.name>
2021-01-17 11:09:31 +0100

    log_func and f string

--- space.py
+++ space.py
@@ -10,23 +10,23 @@ class Space:
     @log_func(logger)
     def add_size(self, id):
         try:
-            img_size = os.path.getsize("{}{}.jpg".format(self.img_path, id))
-            logger.debug("img_size: {}".format(img_size))
+            img_size = os.path.getsize(f"{self.img_path}{id}.jpg")
+            logger.debug(f"img_size: {img_size}")
         except FileNotFoundError as e:
-            logger.warning('Could not find image file: {}'.format(e))
+            logger.warning(f'Could not find image file: {e}')
 
             try:
                 # Sleeps two seconds if the OS is late
                 sleep(2)
 
-                img_size = os.path.getsize("{}{}.jpg".format(self.img_path, id))
+                img_size = os.path.getsize(f"{self.img_path}{id}.jpg")
             except FileNotFoundError as e:
-                logger.warning('Could not find image attempt two file: {}'.format(e))
+                logger.warning(f'Could not find image attempt two file: {e}')
 
                 img_size = 0
         finally:
             self.total_image_data_size += img_size
-            logger.debug("total imge data: {}".format(self.total_image_data_size))
+            logger.debug(f"total imge data: {self.total_image_data_size}")
 
     @log_func(logger)
     def is_space_left(self,):
